7095378580629ad5fccff68eba782f59805fc770
Jovani Benavides <xavibenavides@yahoo.com>
2023-03-20 19:04:04 -0700

    commit

--- COMP003B.Assignment1/Program.cs
+++ COMP003B.Assignment1/Program.cs
@@ -8,7 +8,7 @@ namespace COMP003B.Assignment1
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+             
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
